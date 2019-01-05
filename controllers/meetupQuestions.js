9a477bfc361c1162bfe9ac5b32b3c44912c5a962
ViaChristopheH <viachris.hab@gmail.com>
2019-01-05 20:16:31 +0200

    add feature of creating question for a meetup

--- controllers/meetupQuestions.js
+++ controllers/meetupQuestions.js
@@ -1,17 +1,11 @@
 const meetupQuestions = [{
-
-    id​ :​ 4, ​  
-    createdOn :​​ 'Aug 23, 2018​' ,   
-    createdBy​ :​  ​2 , 
-    ​meetup​ :​ 005​ ,    
-    title​ :​ 'student claims'​ , 
-    body​ :​ 'All claims concerning students in the association' ​ , 
-    ​votes​ :​ 31​ 
-    ​
-}]
+   id:4,
+   createdOn:'Aug 23, 2018',
+   createdBy : 2,
+   meetup : 5,
+   title : 'student claims',
+   body : 'all claims concerning students in association',
+   votes : 35,
+}];
 
 export default meetupQuestions;
-
- 
-    ​ 
-    
