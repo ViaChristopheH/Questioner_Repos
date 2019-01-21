94fcb10d7143d64886516a02135d26f1bfa25d54
ViaChristopheH <viachris.hab@gmail.com>
2019-01-21 17:23:38 +0200

    will ultimately update the UI

--- routes/meetRoutes.js
+++ routes/meetRoutes.js
@@ -5,24 +5,18 @@ import meetupRsvpsController from '../controllers/meetupRsvpsController';
 const route = express.Router();
 
 // create a meetup
-route.post('/meetups', meetupfx.createMeetup);
+route.post('/', meetupfx.createMeetup);
 // get meetup
-route.get('/meetups', meetupfx.fetchAllMeetups);
-// get meetup by id
-route.get('/meetups/:id', meetupfx.fetchAmeetup);
+route.get('/', meetupfx.fetchAllMeetups);
 //get upcoming meetup
-route.get('/meetups/upcoming', meetupfx.fetchUpcomingMeetups);
+route.get('/upcoming', meetupfx.fetchUpcomingMeetups);
+// get meetup by id
+route.get('/:id', meetupfx.fetchAmeetup);
 // create a question
-route.post('/meetups/:id/questions', meetupfx.meetupQuestions);
-// fetch a meetup question
-route.get('/questions', meetupfx.fetchMeetupquestions)
-// upvote a question
-route.patch('/questions/:id/upvote', meetupfx.upvoteQuestion);
-// downvote a question
-route.patch('/questions/:id/downvote', meetupfx.downvoteQuestion);
+route.post('/:id/questions', meetupfx.meetupQuestions);
 // create Rsvps 
-route.post('/meetups/:id/rsvps', meetupRsvpsController.bookingRsvps);
+route.post('/:id/rsvps', meetupRsvpsController.bookingRsvps);
 // Edit Rsvps
-route.put('/meetups/:id/rsvps', meetupRsvpsController.editingRsvps);
+route.put('/:id/rsvps', meetupRsvpsController.editingRsvps);
 
 export default route;
--- test/testApi.js
+++ test/testApi.js
@@ -13,56 +13,63 @@ chai.use(chaiHttp);
         .get('/api/v1/meetups')
         .end((err, res) => {
             res.body.should.be.a('object');
-            res.body.should.have.property('status').eql(200);
-            res.body.should.have.property('data');
-            res.body.data.should.be.a('array');
-           
+            res.body.should.have.property('status').eql(200);           
             done();
         })
       })
   });
 
+
+
   describe('GET/ Specific meetup', () => {
-      it('Should return error meetup id should be a number', (done) => {
+      it('Should return meetup id should be a number', (done) => {
           chai.request(app)
-          .get('api/v1/meetups/INVALID_ID')
+          .get('/api/v1/meetups/1')
           .end((err, res) => {
-              res.should.have.status(400);
-              res.body.error.should.be.eql('ID must be a number');
+              res.body.should.be.a('object');
+              res.body.should.have.property('status').eql(200);
               done();
           });
       });
   });
 
- 
-  describe('get a specific meetup', () => {
-     it ('/GET /meetupsByID', (done) =>{
+  describe('Fetch all upcoming meetups', ()=>{
+    it('/GET /meetup/upcoming', (done)=>{
+         
 
-        chai.request(app)
-            .get('/api/v1/meetups/1')
-            .end((err, res) =>{
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('status').eql(200);
-                res.body.should.have.property('data');
-                res.body.data.should.be.a('array');
-                done();
-                
+       chai.request(app)
+            .get('/api/v1/meetups/upcoming')
+            .end((err, res)=>{
 
+               res.body.should.be.a('object');
+               res.body.should.have.property('status').eql(200);
+               res.body.should.have.property('data');
+               res.body.data.should.be.a('array');
+               res.body.data[0].should.have.property('id').eql(2);
+               res.body.data[0].should.have.property('createdOn');
+               res.body.data[0].should.have.property('topic').eql('Luxury things');
+               res.body.data[0].should.have.property('location').eql('Gasabo');
+               res.body.data[0].should.have.property('happeningOn').eql('February 19, 2019');
+               res.body.data[0].should.have.property('Tags');
+               res.body.data[0].Tags.should.be.a('array');
+               done();
             })
-     })
- });
 
+
+
+    })
+});
+
+ 
   describe('increase by 1 to vote points of a meetup question', () => {
      it('PATCH /questions/<question-id>/upvote', (done) => {
-
         chai.request(app)
-            .patch('/api/v1/questions/1/upvote')
+            .patch('/api/v1/questions/4/upvote')
             .end((err, res) => {
                 res.body.should.be.a('object');
-                res.body.should.have.property('status').eql(201);
+                res.should.have.status(201);
                 res.body.should.have.property('data');
-                res.body.should.be.a('array');
+                res.body.data.should.be.a('array');
                 
                done();
               })
@@ -76,7 +83,7 @@ describe('Decrease by 1 to vote points of a meetup question', () => {
     it('PATCH  /questions/<question-id>/downvote', (done) => {
 
         chai.request(app)
-            .patch('/api/v1/questions/1/downvote')
+            .patch('/api/v1/questions/4/downvote')
             .end((err, res) => {
                 res.body.should.be.a('object');
                 res.body.should.have.property('status').eql(201);
@@ -86,4 +93,39 @@ describe('Decrease by 1 to vote points of a meetup question', () => {
                done();
             })
     })
-})
+});
+
+describe('create a meetup question', () => {
+    it('POST  /questions/', (done) => {
+const questionData = {
+    title: 'Market rise', 
+    body: 'Can the price be revised?'
+}
+        chai.request(app)
+            .post('/api/v1/questions/')
+            .send(questionData)
+            .end((err, res) => {
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql(201);
+                res.body.should.have.property('data');
+                res.body.data.should.be.a('array');
+                
+               done();
+            })
+    })
+
+    it('should not create a question without a body', (done) => {
+        const questionData = {
+            title: 'Market rise' 
+        }
+                chai.request(app)
+                    .post('/api/v1/questions/')
+                    .send(questionData)
+                    .end((err, res) => {
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('status').eql(400);
+                        res.body.should.have.property('error').eql('\"body\" is required'); 
+                       done();
+                    })
+            })
+});
